f43028dd85992fcf8916706acb615992c681bfda
GauharY <143212488+GauharY@users.noreply.github.com>
2026-04-28 15:17:55 +0530

    Create RoutingSetUp.jsx

---
+++ frontend/src/components/RoutingSetUp.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Booking from "./pages/Booking";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
